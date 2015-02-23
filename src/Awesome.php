18c251028de6c684b48b2a8f27d07b343fd356dc
Blaž Oražem <blaz@orazem.info>
2015-02-23 22:22:47 +0100

    debug functions added

--- src/Awesome.php
+++ src/Awesome.php
@@ -8,4 +8,14 @@ class Awesome {
 		echo 'Awesome dude!';
 	}
 
+	public static function debug($var)
+	{
+		print("<pre>" . print_r($var, true) . "</pre>");
+	}
+
+	public static function diebug($var)
+	{
+		exit(self::debug($var));
+	}
+
 }
